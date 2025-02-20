35d89cd55f8d743cc1838924c34f897ec348495c
Jorge Chavez Huincho <djjmygm160399@gmail.com>
2025-02-20 14:18:27 -0500

    terminado el logueo y mostrando sidebar y cerrando sesion

--- app/urls.py
+++ app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('signin/', views.signin, name='signin'),
     path('signup/', views.signup, name='signup'),
+    path('signout/', views.signout, name='signout'),
 ]
