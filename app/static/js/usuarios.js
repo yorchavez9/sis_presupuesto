d2f4af057c3b34999edeea4292e6fe2f8c6fe9e6
Jorge Chavez Huincho <djjmygm160399@gmail.com>
2025-02-25 09:09:27 -0500

    Creando nuevo usuario

--- app/static/js/usuarios.js
+++ app/static/js/usuarios.js
@@ -14,7 +14,6 @@ $(document).ready(function() {
             type: 'GET',
             dataType: 'json',
             success: function(usuarios) {
-                console.log("Datos recibidos:", usuarios);
                 tabla.clear();
                 usuarios.forEach(function(dato, index) {
                     tabla.row.add([
@@ -47,4 +46,88 @@ $(document).ready(function() {
     }
 
     cargarUsuarios();
+
+    /* =========================================
+    CREAR USUARIO
+    ========================================= */
+    $("#btn_guardar_usuario").click(function() {
+        let isValid = true;
+
+        let first_name = $("#first_name").val();
+        let email = $("#email").val();
+        let username = $("#username").val();
+        let password1 = $("#password1").val();
+        let password2 = $("#password2").val();
+
+        if (first_name == "" || first_name == null) {
+            $("#error_first_name_usuario").html("El nombre es obligatorio");
+            isValid = false;
+        } else {
+            $("#error_first_name_usuario").html("");
+        }
+
+        if (email == "" || email == null) {
+            $("#error_email_usuario").html("El correo es obligatorio");
+            isValid = false;
+        } else {
+            $("#error_email_usuario").html("");
+        }
+
+        if (username == "" || username == null) {
+            $("#error_username_usuario").html("El usuario es obligatorio");
+            isValid = false;
+        } else {
+            $("#error_username_usuario").html("");
+        }
+
+        if (password1 == "" || password1 == null) {
+            $("#error_password1_usuario").html("La contraseña es obligatoria");
+            isValid = false;
+        } else if (password1.length < 6) {
+            $("#error_password1_usuario").html("La contraseña debe tener al menos 6 caracteres");
+            isValid = false;
+        } else {
+            $("#error_password1_usuario").html("");
+        }
+
+        if (password2 == "" || password2 == null) {
+            $("#error_password2_usuario").html("Confirmar la contraseña es obligatorio");
+            isValid = false;
+        } else if (password1 !== password2) {
+            $("#error_password2_usuario").html("Las contraseñas no coinciden");
+            isValid = false;
+        } else {
+            $("#error_password2_usuario").html("");
+        }
+
+        if (isValid) {
+            const datos  = new FormData();
+            datos.append("first_name", first_name);
+            datos.append("email", email);
+            datos.append("username", username);
+            datos.append("password1", password1);
+            datos.append("password2", password2);
+
+            $.ajax({
+                url: "crear/",
+                type: 'POST',
+                data: datos,
+                processData: false,
+                contentType: false,
+                success: function(response) {
+                    if (response.status) {
+                        cargarUsuarios();
+                        $("#modal_nuevo_usuario").modal("hide");
+                        $("#form_crear_usuario")[0].reset();
+                    } else {
+                        alert(response.message);
+                    }
+                },
+                error: function(error) {
+                    console.error("Error al crear usuario:", error);
+                }
+            })
+        }
+    });
+    
 });
