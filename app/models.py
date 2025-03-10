e44b576c2b706cb3640915191a660b703fb8d7d6
Jorge Chavez Huincho <djjmygm160399@gmail.com>
2025-03-10 10:28:59 -0500

    Retomando el proyecto

--- app/models.py
+++ app/models.py
@@ -153,3 +153,19 @@ class MaterialServicio(models.Model):
 
 class Tareas(models.Model):
     nombre = models.CharField(max_length=50)
+    
+    
+
+class Regalo(models.Model):
+    nombre = models.CharField(max_length=100, verbose_name="Nombre del Regalo")
+    precio = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Precio")
+    descripcion = models.TextField(null=True, blank=True, verbose_name="Descripción")
+    fecha = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de Registro")
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        verbose_name = "Regalo"
+        verbose_name_plural = "Regalos"
+        db_table = "regalos"  # Nombre de la tabla en la base de datos
